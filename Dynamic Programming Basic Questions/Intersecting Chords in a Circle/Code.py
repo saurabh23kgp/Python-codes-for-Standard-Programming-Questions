f04522b1eef6e65b9fdb869456038c966689f964
saurabh23kgp <45493775+saurabh23kgp@users.noreply.github.com>
2020-01-30 22:04:00 +0530

    Create Code.py

---
+++ Dynamic Programming Basic Questions/Intersecting Chords in a Circle/Code.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def chordCnt(self, A):
+        n=2*A
+        ways=[0 for _ in range(n+1)]
+        ways[0]=1
+        ways[2]=1
+        for i in range(4,n+1,2):
+            for j in range(0,i-1,2):
+                ways[i]+=ways[j]*ways[i-2-j]
+        return (ways[n])
