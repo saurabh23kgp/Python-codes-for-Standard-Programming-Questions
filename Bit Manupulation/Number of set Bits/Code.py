131dca3e89d8eb2e855bd87cf59e90903a1514b3
saurabh23kgp <45493775+saurabh23kgp@users.noreply.github.com>
2020-07-23 20:09:32 +0530

    Delete Code.py

--- Bit Manupulation/Number of set Bits/Code.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    # @param A : integer
-    # @return an integer
-    def numSetBits(self, A):
-        num=0
-        for i in range(32):
-            if (A & 1<<i):
-                num+=1
-        return  num
